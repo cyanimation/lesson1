3556abbfa7770152ac42b81ca4f56db6a5202ccd
Miles Larsen <gerritlarsen@yahoo.com>
2018-07-12 22:53:22 -0600

    Original commit in python 3 with instructions

--- basics.py
+++ basics.py
@@ -5,31 +5,33 @@ import os
 import sys
 
 # clear the console screen
-os.system('clear')
+#os.system('clear') for linux, mac
+#os.system('cls') for windows
+os.system('cls')
 
 # print the message
 # the comma at the end ensures that we don't get a newline after the message
-print 'Hello, what is your name? ', 
+print ('Hello, what is your name? ')
 
 # collect the user input into a variable
-name = raw_input()
+name = input()
 
 # print the response using string concatenation
 # the \n is the special character for a newline
-print '\nIt is a pleasure to meet you ' + name + '!\n'
+print ('\nIt is a pleasure to meet you ' + name + '!\n')
 
 # tell the user how long the name is
-print 'Your name is ' + str(len(name)) + ' characters long\n'
+print ('Your name is ' + str(len(name)) + ' characters long\n')
 
-print 'How old are you? ',
+print ('How old are you? ')
 
 # convert the input to a number (this could fail if the input isn't a number)
-age = int(raw_input())
+age = int(input())
 
-print '\nAnd have you already had a birthday this year (y/n)? ',
+print ('\nAnd have you already had a birthday this year (y/n)? ')
 
 # get the answer concerning the past birthday and save it as a boolean
-past_birthday = raw_input()
+past_birthday = input()
 
 # change the 'y' or 'n' to a True or False
 if past_birthday == 'y':
@@ -39,8 +41,8 @@ elif past_birthday == 'n':
     past_birthday = False
 else:
     # make sure that you use the right quotes (' vs ")
-    print "\nI didn't understand your response."
-    raw_input('\nPress enter to quit...')
+    print ("\nI didn't understand your response.")
+    input('\nPress enter to quit...')
     # clear the console screen
     os.system('clear')
     # exit the program
@@ -50,13 +52,13 @@ else:
 # an 'if' statement can be on a single line like this
 # The parentheses are to enforce a specific order of operations. Without them
 # this expression will not evaluate correctly.
-year = 2015 - age - (0 if past_birthday else 1)
+year = 2018 - age - (0 if past_birthday else 1)
 
 # conver the number back to a string because you can't add a number and a string
-print '\nI know what year you were born in. It was ' + str(year) + '.'
+print ('\nI know what year you were born in. It was ' + str(year) + '.')
 
 # wait for the user to press enter to quit
-raw_input('\nPress enter to quit...')
+input('\nPress enter to quit...')
 
 # clear the console screen
-os.system('clear')
+os.system('cls')
--- gpa_calculator.py
+++ gpa_calculator.py
@@ -1,3 +1,21 @@
+"""
+Write a program that does the following:
+    create a variable for each class you want to include (biology, geometry, etc)
+    create variables storing the credits for each grade
+    a = 4
+    a_minus = 3.7
+    ...
+    ask the user what letter grade they received for each class you chose to include
+    calculate the points for each grade by multiplying the user's provided value with the credits associated with that letter grade
+
+    if (biology == 'a-'):
+        biology_points = a_minus * biology ...
+    keep track of the total points and the total credits
+    calculate the GPA: total_points / total_credits
+    print your cumulative GPA
+"""
+
+
 
 # the points per letter grade
 a = 4
@@ -10,7 +28,7 @@ credits = 3
 classes_num = 1
 
 # the letter grade for the class
-biology = raw_input("what grade did you get in biology (a, a-, b+, etc)?")
+biology = input("what grade did you get in biology (a, a-, b+, etc)?")
 
 # keep track of the total number of points from every class operation
 total_points = 0
@@ -27,10 +45,10 @@ if (biology == 'a'):
     # its a shortcut
     total_points += points
 else:
-    print "I didn't understand that"
+    print ("I didn't understand that")
 
 # calculate the total credits
 total_credits = credits * classes_num
 
 # print out the GPA
-print "your GPA is: " + str(total_points / total_credits)
+print ("your GPA is: " + str(total_points / total_credits))
