8e6f2bfe722b78d414e4d3512b247b0cfa3053b1
Miles Larsen <gerritlarsen@yahoo.com>
2018-07-12 22:38:39 -0600

    Original commit in python 3 with instructions

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,17 @@
-
-tip = 20.0
+"""
+Write a program that receives the following inputs from the user:
+    the server's name
+    how much the meal cost
+    the tax rate as a floating point (e.g. 6.5)
+    the tip percent as an integer (e.g. 15 or 20)
+Print out the following results:
+    The amount to tip
+    The total amount for the meal
+    A thank you for the server
+"""
+tip = 20
 total = 30.0
 server = 'John'
-print "The tip amount is: $" + str(tip)
-print "The total amount for my meal is: $" + str(total)
-print "Thanks for your service " + server
+print( "The tip amount is: $" + str(tip))
+print("The total amount for my meal is: $" + str(total))
+print ("Thanks for your service " + server)
